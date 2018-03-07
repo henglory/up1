b40cf6a6b5ea06caa142dce6b8254cb79eb4e4b4
IRONMAN <ironman@IRONMANs-Mac-mini.local>
2018-03-07 11:13:50 +0700

    clean up

--- toystore/src/test/java/toystore/repository/PersonRepositoryTest.java
+++ toystore/src/test/java/toystore/repository/PersonRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
