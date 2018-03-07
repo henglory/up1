fa9ea9d85fea17ec26e4002fc6dd52ce442709ce
IRONMAN <ironman@IRONMANs-Mac-mini.local>
2018-03-07 14:54:53 +0700

    update domain objects

--- toystore/src/main/java/toystore/domain/Cart.java
+++ toystore/src/main/java/toystore/domain/Cart.java
@@ -3,17 +3,15 @@ package toystore.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document
 public class Cart {
 
     @Id
     private String cartId;
-    private String fullName;
-    private String address1;
-    private String address2;
-    private String city;
-    private String province;
-    private String postCode;
+    private List<CartItem> items;
+    private ShippingInfo shippingInfo;
 
     public String getCartId() {
         return cartId;
@@ -23,51 +21,19 @@ public class Cart {
         this.cartId = cartId;
     }
 
-    public String getFullName() {
-        return fullName;
+    public List<CartItem> getItems() {
+        return items;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setItems(List<CartItem> items) {
+        this.items = items;
     }
 
-    public String getAddress1() {
-        return address1;
+    public ShippingInfo getShippingInfo() {
+        return shippingInfo;
     }
 
-    public void setAddress1(String address1) {
-        this.address1 = address1;
-    }
-
-    public String getAddress2() {
-        return address2;
-    }
-
-    public void setAddress2(String address2) {
-        this.address2 = address2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
+    public void setShippingInfo(ShippingInfo shippingInfo) {
+        this.shippingInfo = shippingInfo;
     }
 }
---
+++ toystore/src/main/java/toystore/domain/ShippingInfo.java
@@ -0,0 +1,59 @@
+package toystore.domain;
+
+public class ShippingInfo {
+
+    private String fullName;
+    private String address1;
+    private String address2;
+    private String city;
+    private String province;
+    private String postCode;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+}
