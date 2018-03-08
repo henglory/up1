57c6710c97507699a7a7bc3fa78b32435d9aec81
Nathas Yingsukamol <new08242@gmail.com>
2018-03-08 12:03:48 +0700

    change endpoint

--- src/constants/ApiConstant.js
+++ src/constants/ApiConstant.js
@@ -1,10 +1,18 @@
 
     
-var endpoint = "http://www.mocky.io"
+// var endpoint = "http://www.mocky.io"
+
+// const ApiConstant = {
+
+//     HELLO_ENDPOINT: endpoint + "/v2/5a9f5fcd2e00002a0074d054"
+
+// }
+
+var endpoint = "http://46.101.48.109/api"
 
 const ApiConstant = {
 
-    HELLO_ENDPOINT: endpoint + "/v2/5a9f5fcd2e00002a0074d054"
+    HELLO_ENDPOINT: endpoint + "/hello/data/somkiat"
 
 }
 
